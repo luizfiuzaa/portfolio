40e11e2dbcc8553ac9ede4f70842a9d0f5936247
luizfiuzaa <luizffreitas2006@gmail.com>
2024-02-09 05:02:02 -0300

    ajustar responsividade

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  themeToggle:any = false;
+
+  selectSegment:any = 'about';
+
+  ngOnInit() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    this.initializeDarkTheme(prefersDark.matches);
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkTheme(mediaQuery.matches));
+  }
+
+  initializeDarkTheme(isDark:any) {
+    this.themeToggle = isDark;
+    this.toggleDarkTheme(isDark);
+  }
+
+  toggleChange(event:any) {
+    this.toggleDarkTheme(event.detail.checked);
+  }
+
+  toggleDarkTheme(shouldAdd:any) {
+    document.body.classList.toggle('dark', shouldAdd);
+  }
+
+
 
 }
