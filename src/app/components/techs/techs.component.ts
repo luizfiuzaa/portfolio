576906c41755d0cdc947499f4c1de02d6a107154
luizfiuzaa <luizffreitas2006@gmail.com>
2024-05-28 16:13:15 -0300

    $

--- src/app/components/techs/techs.component.ts
+++ src/app/components/techs/techs.component.ts
@@ -7,50 +7,55 @@ import { Techs } from 'src/app/interface/techs';
   styleUrls: ['./techs.component.scss'],
 })
 export class TechsComponent implements OnInit {
-
-
-  techs: Techs[] = [{
-    name: 'Angular',
-    logo: 'logo-angular',
-    color: 'danger',
-  }, {
-    name: 'Ionic Framewrok',
-    logo: 'logo-ionic',
-    color: 'primary',
-  }, {
-    name: 'JavaScript',
-    logo: 'logo-javascript',
-    color: 'warning',
-  }, {
-    name: 'Sass',
-    logo: 'logo-sass',
-    color: 'danger',
-  }, {
-    name: 'Bootstrap',
-    logo: 'layers',
-    color: 'tertiary',
-  }, {
-    name: 'PHP',
-    logo: 'server',
-    color: 'tertiary',
-  }, {
-    name: 'MySQL',
-    logo: 'server',
-    color: 'secondary',
-  },{
-    name: 'Git/GitHub',
-    logo: 'logo-github',
-    color: 'dark',
-  },
-  {
-    name: 'Figma',
-    logo: 'logo-figma',
-    color: 'tertiary',
-  },
+  techs: Techs[] = [
+    {
+      name: 'Angular',
+      logo: 'logo-angular',
+      color: 'danger',
+    },
+    {
+      name: 'Ionic Framewrok',
+      logo: 'logo-ionic',
+      color: 'primary',
+    },
+    {
+      name: 'Sass',
+      logo: 'logo-sass',
+      color: 'danger',
+    },
+    {
+      name: 'PHP',
+      logo: 'server',
+      color: 'tertiary',
+    },
+    {
+      name: 'MySQL',
+      logo: 'server',
+      color: 'secondary',
+    },
+    {
+      name: 'JavaScript',
+      logo: 'logo-javascript',
+      color: 'warning',
+    },
+    {
+      name: 'Bootstrap',
+      logo: 'layers',
+      color: 'tertiary',
+    },
+    {
+      name: 'Figma',
+      logo: 'logo-figma',
+      color: 'tertiary',
+    },
+    {
+      name: 'Git/GitHub',
+      logo: 'logo-github',
+      color: 'dark',
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
-
+  ngOnInit() {}
 }
